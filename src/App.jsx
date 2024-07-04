dc7311d3875d2e116f0f5911d946386a47d75dd7
Hariom dangi <hdangi224@gmail.com>
2024-07-04 20:06:21 +0530

    form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
-// import React, { useState } from "react";
+// import React from "react";
 
 // const App = () => {
-//   const [list, setList] = useState(["Akshay", "Nikhil", "Avinash", "Manav"]);
+//   const SubmitHandler = (e) => {
+//     e.preventDefault();
 
-//   let listRendring = list.map((list, index)=>{
-//     return (
-//       <li key={index}>
-//        {index+1}. {list}
-//       </li>
-//     )
-//   })
+//     const { name, email, phone } = e.target;
+
+//     const user = {
+//       name: name.value,
+//       email: email.value,
+//       phone: phone.value,
+//     };
+
+//     console.log(user);
+//   };
 
 //   return (
 //     <>
-//       <main className="p-10">
-//         <h1 className="font-bold text-3xl">List Rendring</h1>
+//       <h1 className="text-3xl font-bold mt-10 text-center">Form Handling</h1>
 
-//         <ul className="m-10">
-//          {listRendring}
-//         </ul>
-//       </main>
+//       <form
+//         className="mt-10 flex items-center justify-center flex-col"
+//         onSubmit={SubmitHandler}
+//       >
+//         <input type="text" name="name" placeholder="Enter your name" />
+//         <input type="email" name="email" placeholder="Enter your email" />
+//         <input type="number" name="phone" placeholder="Enter your phone" />
+//         <button type="submit">Submit</button>
+//       </form>
 //     </>
 //   );
 // };
@@ -29,36 +37,53 @@
 // import React, { useState } from "react";
 
 // const App = () => {
-//   const [list, setList] = useState([
-//     {
-//       id: "101",
-//       name: "Akshay",
-//     },
-//     {
-//       id: "102",
-//       name: "Abhinav",
-//     },
-//     {
-//       id: "103",
-//       name: "Akash",
-//     },
-//   ]);
+//   const [name, setName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [phone, setPhone] = useState("");
 
-//   console.log(list);
+//   const SubmitHandler = (e) => {
+//     e.preventDefault();
+
+//     const user = {
+//       name,
+//       email,
+//       phone,
+//     };
+
+//     console.log(user);
+//   };
 
 //   return (
 //     <>
-//       <main className="p-10">
-//         <h1 className="font-bold text-3xl">List Rendring</h1>
+//       <h1 className="text-3xl font-bold mt-10 text-center">Form Handling</h1>
 
-//         <ul className="m-10">
-//           {list.map((list, index) => (
-//             <li key={index}>
-//              {list.id}. {list.name}
-//             </li>
-//           ))}
-//         </ul>
-//       </main>
+//       <form
+//         className="mt-10 flex items-center justify-center flex-col"
+//         onSubmit={SubmitHandler}
+//       >
+//         <input
+//           type="text"
+//           placeholder="Enter your name"
+//           value={name}
+//           onChange={(e) => setName(e.target.value)}
+//           required
+//         />
+//         <input
+//           type="email"
+//           placeholder="Enter your email"
+//           value={email}
+//           onChange={(e) => setEmail(e.target.value)}
+//           required
+//         />
+//         <input
+//           type="number"
+//           placeholder="Enter your phone"
+//           value={phone}
+//           onChange={(e) => setPhone(e.target.value)}
+//           required
+//         />
+//         <button type="submit">Submit</button>
+//       </form>
 //     </>
 //   );
 // };
@@ -66,70 +91,58 @@
 // export default App;
 
 import React, { useState } from "react";
-import { Bounce, toast } from "react-toastify";
 
 const App = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-
-    if (name === "") {
-      toast.error("Name is required !", {
-        position: "top-right",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-        transition: Bounce,
-      });
-      return;
-    }
-
-    if (email === "") {
-      toast.error("Email is required !", {
-        position: "top-right",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-        transition: Bounce,
-      });
-      return;
-    }
-
-    console.log(name, email);
+  const handleInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
   };
 
-  return (
-    <main className="p-10">
-      <h1 className="font-bold text-3xl">Form Validation</h1>
+  const SubmitHandler = (e) => {
+    e.preventDefault();
 
-      <form onSubmit={submitHandler}>
+    console.log(formData);
+  };
+  return (
+    <>
+      <h1 className="text-3xl font-bold mt-10 text-center">Form Handling</h1>
+
+      <form
+        className="mt-10 flex items-center justify-center"
+        onSubmit={SubmitHandler}
+      >
         <input
           type="text"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          name="name"
+          value={formData.name}
+          onChange={handleInputChange}
+          placeholder="Enter your name"
         />
-        <br />
         <input
           type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+          placeholder="Enter your email"
         />
-        <br />
+        <input
+          type="number"
+          name="phone"
+          value={formData.phone}
+          onChange={handleInputChange}
+          placeholder="Enter your phone"
+        />
         <button type="submit">Submit</button>
       </form>
-    </main>
+    </>
   );
 };
 
